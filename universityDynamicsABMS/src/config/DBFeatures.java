ea7340d11267f0778d8d7b3befe2e98c9bf151a0
David Romero <romeromillandavidandres@gmail.com>
2020-06-25 08:09:13 -0500

    commenting

--- universityDynamicsABMS/src/config/DBFeatures.java
+++ universityDynamicsABMS/src/config/DBFeatures.java
@@ -2,35 +2,74 @@ package config;
 
 public class DBFeatures {
 
-	// Groups database
+	/**
+	 * Groups database - subject column
+	 */
 	public static final int SUBJECT_COLUMN = 0;
 
+	/**
+	 * Groups database - group column
+	 */
 	public static final int GROUP_COLUMN = 1;
 
+	/**
+	 * Groups database - day column
+	 */
 	public static final int DAY_COLUMN = 2;
 
+	/**
+	 * Groups database - start time column
+	 */
 	public static final int START_TIME_COLUMN = 3;
 
+	/**
+	 * Groups database - end time column
+	 */
 	public static final int END_TIME_COLUMN = 4;
 
+	/**
+	 * Groups database - capacity column
+	 */
 	public static final int CAPACITY_COLUMN = 5;
 
+	/**
+	 * Groups database - teaching facility column
+	 */
 	public static final int TEACHING_FACILITY_COLUMN = 6;
 
+	/**
+	 * Groups database - room column
+	 */
 	public static final int ROOM_COLUMN = 7;
 
-	// Routes database
+	/**
+	 * Routes database - origin column
+	 */
 	public static final int ORIGIN_COLUMN = 0;
 
+	/**
+	 * Routes database - destination column
+	 */
 	public static final int DESTINATION_COLUMN = 1;
 
+	/**
+	 * Routes database - distance column
+	 */
 	public static final int DISTANCE_COLUMN = 2;
 
-	// Facility areas database
+	/**
+	 * Facility areas database - facility id column
+	 */
 	public static final int FACILITY_ID = 0;
 
+	/**
+	 * Facility areas database - facility area column
+	 */
 	public static final int FACILITY_AREA = 1;
 
+	/**
+	 * Facility areas database - facility weight column
+	 */
 	public static final int FACILITY_WEIGHT = 2;
 
 }
