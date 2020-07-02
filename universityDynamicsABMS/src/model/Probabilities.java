998299cabc71e273caae2dcd889764463ab7587f
David Andrés Romero Millán <romeromillandavidandres@gmail.com>
2020-07-02 09:57:48 -0500

    organize imports

--- universityDynamicsABMS/src/model/Probabilities.java
+++ universityDynamicsABMS/src/model/Probabilities.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import cern.jet.random.Normal;
-import gis.GISDensityMeter;
 import gis.GISPolygon;
 import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.parameter.Parameters;
